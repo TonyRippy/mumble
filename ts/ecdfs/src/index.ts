05a07bfe892588a0e2eacb7abd9bddfffc090dd5
Tony Rippy <tonyrippy@gmail.com>
2023-03-10 22:22:54 -0500

    Add license header to typescript source files.

--- ts/ecdfs/src/index.ts
+++ ts/ecdfs/src/index.ts
@@ -1,3 +1,19 @@
+// A typescript library for loading and manipulating ECDFs.
+// Copyright (C) 2023, Tony Rippy
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License in the LICENSE file at the
+// root of this repository, or online at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 export { Func, linearFunction } from './func';
 export { ECDF } from './ecdf';
 export { Layer, CDFGraph, getRenderFuncForPlot, getRenderFuncForFunction } from "./plot";
