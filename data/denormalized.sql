54850da52c8b0863510c49f677bd2bcab4b6916c
Tony Rippy <tonyrippy@gmail.com>
2023-06-04 10:29:44 -0400

    Add Sqlite schema for clustering tests.

---
+++ data/denormalized.sql
@@ -0,0 +1,17 @@
+CREATE TABLE [label_set] (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   labels TEXT NOT NULL
+);
+
+INSERT INTO [label_set] (labels) VALUES ('{}');
+
+CREATE TABLE [monitoring_data] (
+   timestamp DATETIME NOT NULL,
+   label_set_id INTEGER NOT NULL,
+   data BLOB NOT NULL,
+
+   PRIMARY KEY (timestamp, label_set_id),
+
+   FOREIGN KEY (label_set_id) REFERENCES [label_set] (id) 
+     ON DELETE CASCADE ON UPDATE NO ACTION
+);
---
+++ data/normalized.sql
@@ -0,0 +1,33 @@
+CREATE TABLE [label_set] (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   labels TEXT NOT NULL
+);
+
+INSERT INTO [label_set] (labels) VALUES ('{}');
+
+CREATE TABLE [cluster_group] (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   config TEXT NOT NULL
+);
+
+INSERT INTO [cluster_group] (config) VALUES ('{}');
+
+CREATE TABLE [cluster] (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   group_id INTEGER NOT NULL,
+   centroid BLOB NOT NULL,
+
+   FOREIGN KEY (group_id) REFERENCES [cluster_group] (id)
+);
+
+CREATE TABLE [monitoring_data] (
+   time TIMESTAMP NOT NULL,
+   label_set_id INTEGER NOT NULL,
+   cluster_id INTEGER NOT NULL,
+   count INTEGER NOT NULL,
+
+   PRIMARY KEY (time, label_set_id),
+
+   FOREIGN KEY (label_set_id) REFERENCES [label_set] (id),
+   FOREIGN KEY (cluster_id) REFERENCES [cluster] (id)
+);
